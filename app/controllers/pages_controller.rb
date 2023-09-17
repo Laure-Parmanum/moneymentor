ac2570b0935d77d739f80dd477590cb6a7505e0f
Tirouvalen Rungiah <tirouvalen.rungiah@gmail.com>
2023-09-17 17:17:59 +0400

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # @targets = Target.all
+    # @balances = Balance.all
   end
 end
