bcfe83e7a26716304ddfa664e9ba80d30873ca6b
Tirouvalen Rungiah <tirouvalen.rungiah@gmail.com>
2023-10-07 15:45:23 +0400

    updated the balance show

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -14,6 +14,7 @@ class BalancesController < ApplicationController
   def show
     # @balances = Balance.all
     # @targets = TargetAmount.all
+    @transactions = @balance.transactions
   end
 
   def new
